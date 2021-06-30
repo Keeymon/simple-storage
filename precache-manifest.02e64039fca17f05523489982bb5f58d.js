34c2d75f196de675bdc9943aa2db836c52dfd832
Yann Pichereau <yann_pichereau@hotmail.fr>
2021-06-30 10:27:14 +0200

    Updates

---
+++ precache-manifest.02e64039fca17f05523489982bb5f58d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31bc8b7587e62383429b070bfa907f3",
+    "url": "/simple-storage/index.html"
+  },
+  {
+    "revision": "5bf42a12fd6ff6d44d47",
+    "url": "/simple-storage/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ecc7dc6f80a3f1dfe314",
+    "url": "/simple-storage/static/js/2.dbaa57b2.chunk.js"
+  },
+  {
+    "revision": "5bf42a12fd6ff6d44d47",
+    "url": "/simple-storage/static/js/main.12f19691.chunk.js"
+  },
+  {
+    "revision": "393911a39966e07e4fca",
+    "url": "/simple-storage/static/js/runtime-main.5a136975.js"
+  }
+]);
